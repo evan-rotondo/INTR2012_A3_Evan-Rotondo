648f1828e23ada62b466feafef82ffa815dc36b8
evan-rotondo <71403068+evan-rotondo@users.noreply.github.com>
2021-01-31 17:22:59 -0500

    first commit-test

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function  start(){
+    
+   // $("body").css("background-color", "pink");
+}
+
+$(window).on("load", start);
+
+
+function loadpage(e){
+   
+   e.preventDefault();
+   $("#menu a.active").removeClass("active");
+   $(this).addClass("active");
+    //alert("!");
+
+  $("#content").load("about.html");
+}
+
+$(document).on("click", "#menu a", loadpage);
+
+
+
+
